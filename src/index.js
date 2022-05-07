a0f7ef4094c28f51668a0d4d81434cbdff6eed69
Arpan Mukhopadhyay <arixion@outlook.com>
2022-05-07 18:45:38 +0530

    updated css colors

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ for (var i = 0; i < 100; i++) {
   fWrapper.append(`
     <div class="card col-12 app-feature-card border-0 mb-3">
       <div class="card-body d-flex flex-row">
-        <div class="me-2">
+        <div class="me-3">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-check-circle-fill text-success" viewBox="0 0 16 16">
             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
           </svg>
